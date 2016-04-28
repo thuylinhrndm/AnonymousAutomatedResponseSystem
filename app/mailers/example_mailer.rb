403a134e119d7f8202cd7cfcebc10dc455a49dd5
thuylinh <thuylinhrndm@gmail.com>
2016-04-28 18:59:02 +0000

    style sheet

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -2,11 +2,11 @@ class ExampleMailer < ActionMailer::Base
   default from: "noreply@cloudencounter2.edu"
 
   def sample_mailer
-  	mail(to: 'rndmukireland@gmail.com', subject: 'This is an email from CloudEncounter!')
+  	mail(to: 'rndmukireland@gmail.com', subject: 'This is an email from AARS!')
   end
 
   def send_email_to_consultant(consultant_email_address, message)
   	@message = message
-  	mail(to: consultant_email_address, subject: 'Email from a CloudEncounter User', )
+  	mail(to: consultant_email_address, subject: 'Email from a AARS User', )
   end
 end
--- app/mailers/reminder.rb
+++ app/mailers/reminder.rb
@@ -1,10 +1,10 @@
 class Reminder < ActionMailer::Base
-  default from: "reminders@cloudencounter.com"
+  default from: "reminders@aars.com"
    
    def reminder_email(user_id, appointment_hour_time, consultant_id)
     user = User.find(user_id)
     consultant = User.find(consultant_id)
-    @message = "Hi #{user.name},\nYou have an appointment tomorrow at #{appointment_hour_time} with #{consultant.name}. \n All the best,\nCloudEncounter" 
+    @message = "Hi #{user.name},\nYou have an appointment tomorrow at #{appointment_hour_time} with #{consultant.name}. \n All the best,\nAARS" 
     mail(to: user.email, subject: "Reminder of your appointment tomorrow")
    end
    
